297d9855d9f232ef4243b2aa21c585d706eead52
octadion <dyonoct@gmail.com>
2023-08-19 21:22:02 +0700

    update

--- api/index.ts
+++ api/index.ts
@@ -10,7 +10,7 @@ createConnection().then(() => {
     app.use(express.json());
     app.use(cookieParser());
     app.use(cors({
-        origin: ['https://register-expressjs.vercel.app/'],
+        origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200'],
         credentials: true
     }));
 
